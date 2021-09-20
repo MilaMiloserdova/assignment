8a28529743c1efa71229935e6f38eb958767b18e
Mila Miloserdova <90848407+MilaMiloserdova@users.noreply.github.com>
2021-09-21 00:50:52 +0300

    adding mathemetical operations
    Ask for a number to show a trick

---
+++ hometask.py
@@ -0,0 +1,15 @@
+task = input('I can turn any number you give into 4')
+print ('Give me a number')
+a = int(input())
+print ('Let us multiply by 2')
+b = a*2
+print(b)
+print ('Now let us add 8')
+c = b+8
+print(c)
+print ('Let us devide it by 2')
+d = int (c/2)
+print(d)
+print ('Finnaly, we take away the number you initially had in mind')
+t = d-a
+print (t)
